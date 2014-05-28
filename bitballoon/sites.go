aed328f052590d5e502b026b1952c668953f7614
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2014-05-27 22:59:29 -0700

    Remove old Site.deploy methods

--- bitballoon/sites.go
+++ bitballoon/sites.go
@@ -48,12 +48,6 @@ type Site struct {
 	CreatedAt Timestamp `json:"created_at"`
 	UpdatedAt Timestamp `json:"updated_at"`
 
-	// Path to a zip file to deploy
-	Zip string
-
-	// Path to a directory to deploy
-	Dir string
-
 	// Access deploys for this site
 	Deploys *DeployService
 
@@ -95,6 +89,7 @@ func (s *SitesService) List(options *ListOptions) ([]Site, *Response, error) {
 
 	for _, site := range(*sites) {
 		site.client = s.client
+		site.Deploys = &DeployService{client: s.client, site: site}
 	}
 
 	return *sites, resp, err
@@ -112,166 +107,10 @@ func (site *Site) refresh() (*Response, error) {
 }
 
 // Update will update the fields that can be updated through the API
-// If a Zip or a Dir property is present for the site, a new version of the site
-// will be deployed
 func (site *Site) Update() (*Response, error) {
-
-	if site.Zip != "" {
-		return site.deployZip()
-	} else {
-		return site.deployDir()
-	}
-
 	options := &RequestOptions{JsonBody: site.mutableParams()}
 
 	return site.client.Request("PUT", site.apiPath(), options, site)
-}
-
-// Waits for the latest deploy of a site to finish processing
-func (site *Site) WaitForReady(timeout time.Duration) error {
-	if site.State == "current" {
-		return nil
-	}
-
-	if timeout == 0 {
-		timeout = defaultTimeout
-	}
-
-	timedOut := false
-	time.AfterFunc(timeout*time.Second, func() {
-		timedOut = true
-	})
-
-	done := make(chan error)
-
-	go func() {
-		for {
-			time.Sleep(1 * time.Second)
-
-			if timedOut {
-				done <- errors.New("Timeout while waiting for processing")
-				break
-			}
-
-			site, _, err := site.client.Sites.Get(site.Id)
-			if site != nil {
-				fmt.Println("Site state is now: ", site.State)
-			}
-			if err != nil || (site != nil && site.State == "current") {
-				done <- err
-				break
-			}
-		}
-	}()
-
-	err := <-done
-	return err
-}
-
-func (site *Site) deployDir() (*Response, error) {
-	files := map[string]string{}
-
-	err := filepath.Walk(site.Dir, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() == false {
-			rel, err := filepath.Rel(site.Dir, path)
-			if err != nil {
-				return err
-			}
-
-			if strings.HasPrefix(rel, ".") || strings.Contains(rel, "/.") {
-				return nil
-			}
-
-			sha := sha1.New()
-			data, err := ioutil.ReadFile(path)
-
-			if err != nil {
-				return err
-			}
-
-			sha.Write(data)
-
-			files[rel] = hex.EncodeToString(sha.Sum(nil))
-		}
-
-		return nil
-	})
-
-	options := &RequestOptions{
-		JsonBody: &siteUpdate{
-			Name:              site.Name,
-			CustomDomain:      site.CustomDomain,
-			Password:          site.Password,
-			NotificationEmail: site.NotificationEmail,
-			Files:             &files,
-		},
-	}
-
-	deployInfo := new(DeployInfo)
-	resp, err := site.client.Request("PUT", site.apiPath(), options, deployInfo)
-
-	if err != nil {
-		return resp, err
-	}
-
-	lookup := map[string]bool{}
-
-	for _, sha := range deployInfo.Required {
-		lookup[sha] = true
-	}
-
-	for path, sha := range files {
-		if lookup[sha] == true {
-			file, _ := os.Open(filepath.Join(site.Dir, path))
-			defer file.Close()
-
-			options = &RequestOptions{
-				RawBody: file,
-				Headers: &map[string]string{"Content-Type": "application/octet-stream"},
-			}
-			fmt.Println("Uploading %s", path)
-			resp, err = site.client.Request("PUT", filepath.Join(site.apiPath(), "files", path), options, nil)
-			if err != nil {
-				fmt.Println("Error", err)
-				return resp, err
-			}
-		}
-	}
-
-	return resp, err
-}
-
-func (site *Site) deployZip() (*Response, error) {
-	zipPath, err := filepath.Abs(site.Zip)
-	if err != nil {
-		return nil, err
-	}
-
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-
-	fileWriter, err := writer.CreateFormFile("zip", filepath.Base(zipPath))
-	fileReader, err := os.Open(zipPath)
-	defer fileReader.Close()
-
-	if err != nil {
-		return nil, err
-	}
-	io.Copy(fileWriter, fileReader)
-
-	for key, value := range *site.mutableParams() {
-		writer.WriteField(key, value)
-	}
-
-	err = writer.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	contentType := "multipar/form-data; boundary=" + writer.Boundary()
-	options := &RequestOptions{RawBody: body, Headers: &map[string]string{"Content-Type": contentType}}
-
-	return site.client.Request("PUT", site.apiPath(), options, nil)
 }
 
 func (site *Site) mutableParams() *map[string]string {
